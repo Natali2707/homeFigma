ca85707804f4b38e293cc4e6be0307d37793d9b1
Natali2707 <105042840+Natali2707@users.noreply.github.com>
2022-06-10 12:10:39 +0300

    добавила секцию табы, меню вюок, плавный скролл

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,31 @@ document.addEventListener("scroll", () => {
     header.classList.remove("header_has-bg" );
   }
 });
+
+
+
+// function scrollTo(element){
+//   window.scroll(
+//     {
+//       left:0,
+//       top:element.offsetTop,
+//       bottom:element.offsetTop,
+//       behavior: "smooth"
+//     }
+//   )
+// }
+// let contact = document.querySelector(".contact");
+// let contacts = document.querySelector("#contact");
+// contact.addEventListener ("click", () => {
+//   scrollTo(contact);
+// })
+// let pricingScroll = document.querySelector(".pricingScroll");
+// let price = document.querySelector("#pricing");
+// pricingScroll.addEventListener ("click", () => {
+//   scrollTo(pricingScroll);
+// })
+// let reviewScroll = document.querySelector(".reviewScroll");
+// let review = document.querySelector("#reviewScroll");
+// reviewScroll.addEventListener("click", () => {
+//   scrollTo(reviewScroll);
+// })
